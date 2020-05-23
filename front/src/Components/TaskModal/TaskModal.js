a7de27867b447f5999ffb32324ed59e0437dbcbe
Paulo Octavio Araujo <paulooctavio_araujo@hotmail.com>
2020-05-23 16:50:13 -0300

    modal form

--- front/src/Components/TaskModal/TaskModal.js
+++ front/src/Components/TaskModal/TaskModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoMdClose } from 'react-icons/io';
 
 import { StudentsContainer } from '../StudentsContainer/StudentsContainer';
@@ -9,7 +9,24 @@ import './TaskModal.css'
 export const TaskModal = ({toggleModal, activeFilter, studentsList}) => {
 
     const metricsText = `${studentsList.length} aluno${studentsList.length !== 1 ? 's' : ''}`
+    const [topic, setTopic] = useState("");
+    const [deadline, setDeadline] = useState("");
+    const [duration, setDuration] = useState("");
+    const [addtionalInfo, setAddtionalInfo] = useState("");
 
+    const handleTopicChange = (event) => {
+        setTopic(event.target.value)
+    }
+    const handleDeadlineChange = (event) => {
+        setDeadline(event.target.value)
+    }
+    const handleDurationChange = (event) => {
+        setDuration(event.target.value)
+    }
+    const handleAdditionalInfoChange = (event) => {
+        setAddtionalInfo(event.target.value)
+    }
+    
     return (
         <div className='modal-wrapper'>
             <div className='modal-header'>
@@ -26,11 +43,37 @@ export const TaskModal = ({toggleModal, activeFilter, studentsList}) => {
                     showPhone={true}>
                     <NumberOfStudents metricsText={metricsText} />
                 </StudentsContainer>
-                <form>
-                    <label></label><br/>
-                    <input></input>
-                </form>
-                {activeFilter}
+                <div>
+                    <form>
+                        <div className="form-input">
+                            <label>Tópico</label><br/>
+                            <input type="text" onChange={handleTopicChange} value={topic}></input>
+                        </div>
+                        <div className="form-input">
+                            <label>Prazo</label><br/>
+                            <input type="text" onChange={handleDeadlineChange} value={deadline}></input>
+                        </div>
+                        <div className="form-input">
+                            <label>Duração</label><br/>
+                            <input type="text" onChange={handleDurationChange} value={duration}></input>
+                        </div>
+                        <div className="form-task-attachments">
+                            <label>Exercício</label><br/>
+                            <input type="file"></input>
+                        </div>
+                        <div className="form-task-attachments">
+                            <label>Gabarito</label><br/>
+                            <input type="file"></input>
+                        </div>
+                        <div className="form-input">
+                            <label>Informações Adcionais</label><br/>
+                            <textarea onChange={handleAdditionalInfoChange} value={addtionalInfo}></textarea>                          
+                        </div>
+                        <div className="submit-button">
+                            Enviar
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     )

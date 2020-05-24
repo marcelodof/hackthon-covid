3d8ed36c143a463aeeddd4bbbf3ad96815e14c2b
Rafael Rahal <rafaelrahal@Rafaels-MacBook-Pro.local>
2020-05-23 22:17:15 -0300

    change style of selected student

--- front/src/Components/StudentItem/StudentItem.js
+++ front/src/Components/StudentItem/StudentItem.js
@@ -4,14 +4,16 @@ import { FiWifi } from 'react-icons/fi';
 import { GoLocation } from 'react-icons/go'
 import { FaWhatsapp } from 'react-icons/fa';
 import { AiFillPhone } from 'react-icons/ai';
+import { FaCheck } from 'react-icons/fa';
 
 import './StudentItem.css';
 
 export const StudentItem = ({name, hasAddress, hasSmartphone, hasWhatsApp, hasWifi, phone, showPhone, homeworkHasBeenSent}) => {
     return (
-        <div className={`item-container ${homeworkHasBeenSent? 'sent' : null}`}>
+        <div className={`item-container`}>
             <div className="student-name">
                 {name}
+                {homeworkHasBeenSent && <FaCheck style={{color: '#008744'}} className="check-icon" />}
             </div>
             {showPhone 
             ? <div className='student-phone'>

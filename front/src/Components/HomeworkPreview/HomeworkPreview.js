52c23ccdbfc8a452011c4543f62c3af126b73c2b
marcelodof <marcelodof@uol.com.br>
2020-05-24 10:41:43 -0300

    Adding phone text

--- front/src/Components/HomeworkPreview/HomeworkPreview.js
+++ front/src/Components/HomeworkPreview/HomeworkPreview.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { ModalHeader } from '../ModalHeader/ModalHeader';
 import { StudentsContainer } from '../StudentsContainer/StudentsContainer';
 import { HomeworkMessage } from '../HomeworkMessage/HomeworkMessage';
+import { NumberOfStudents } from '../NumberOfStudents/NumberOfStudents';
 
 import './HomeworkPreview.css';
 
@@ -18,19 +19,24 @@ export const HomeworkPreview = (
         homeworkHasBeenSent,
         exerciseFile,
         theoryFile
-    }) => { 
+    }) => {
+        
+    const metricsText = `${studentsList.length} aluno${studentsList.length !== 1 ? 's' : ''}`
+
     return (
     <React.Fragment>
         <ModalHeader toggleModal={toggleModal} headerText={headerText} />
     <div className="preview-wrapper">
             <div className="content-wrapper">
+                <div className="students-preview">
+                    <StudentsContainer studentsList={studentsList} showPhone={true}>
+                        <NumberOfStudents metricsText={metricsText} />
+                    </StudentsContainer>
+                </div>
                 <div className="whatsapp-background">
                     <div className="message-container">
                         <HomeworkMessage topic={topic} deadline={deadline} duration={duration} additionalInfo={additionalInfo} theoryFile={theoryFile} exerciseFile={exerciseFile} />
                     </div>
-                </div>
-                <div className="students-preview">
-                    <StudentsContainer studentsList={studentsList} showPhone={true} />
                 </div>
             </div>
             <div className="button-wrapper">
--- front/src/Components/StudentsContainer/StudentsContainer.js
+++ front/src/Components/StudentsContainer/StudentsContainer.js
@@ -7,21 +7,27 @@ import './StudentsContainer.css'
 export const StudentsContainer = ({studentsList, showPhone, children, hasShadow}) => {
     return (
         <div className={`students-container ${hasShadow ? "shadow" : null}`}>
-            <div className='student-container-list'>
-                {
-                    studentsList.map((student) => {
-                        return <StudentItem
-                                key={student.name}
-                                name={student.name}
-                                hasAddress={student.hasAddress}
-                                hasWhatsApp={student.hasWhatsApp}
-                                hasWifi={student.hasWifi}
-                                phone={student.phone}
-                                showPhone={showPhone}
-                                homeworkHasBeenSent={student.homeworkHasBeenSent}
-                                />
-                    })
-                }
+            <div>
+                {showPhone && 
+                <div className='phone-text'>
+                    Você enviará mensagem para estes alunos:
+                </div>}
+                <div className='student-container-list'>
+                    {
+                        studentsList.map((student) => {
+                            return <StudentItem
+                                    key={student.name}
+                                    name={student.name}
+                                    hasAddress={student.hasAddress}
+                                    hasWhatsApp={student.hasWhatsApp}
+                                    hasWifi={student.hasWifi}
+                                    phone={student.phone}
+                                    showPhone={showPhone}
+                                    homeworkHasBeenSent={student.homeworkHasBeenSent}
+                                    />
+                        })
+                    }
+                </div>
             </div>
             <div className='student-container-bottom'>
                 {children}
